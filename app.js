e79c990f80bd9a1ef1306a57bdf0fb68a598960a
Ljojua15 <jojualasha15@gmail.com>
2024-02-25 00:27:20 +0400

    second-lesson

---
+++ app.js
@@ -0,0 +1,76 @@
+//დავალება 1
+/*
+let number = prompt('check number is odd or even:')
+
+
+if (number % 2 === 0) {
+    console.log('this Number is Even')
+    alert("this Number is Even")
+} else if (number % 2 > 0) {
+    console.log('this is odd')
+    alert("this is odd")
+} else {
+    console.log('please write the number')
+    alert("please write the number")
+}
+*/
+
+//დავალება 2
+/*
+let number = '';
+
+if (number > 0) {
+    console.log('დადებითია')
+} else if (number < 0) {
+    console.log('უარყოფითია')
+} else {
+    console.log('უდრის ნულს')
+}
+*/
+
+//დავეალება 3
+
+
+/*
+let firstNumber = 8
+let secondNumber = 2
+
+if (firstNumber / firstNumber === 1 && firstNumber / 1 === firstNumber && firstNumber % secondNumber > 0) {
+    console.log("მარტივი რიცხვია")
+} else {
+    console.log('იყოფა')
+}
+*/
+
+//დავალება 4
+/*
+let sentence = ''
+
+if (sentence === '') {
+    console.log('ცარიელია');
+} else {
+    console.log(sentence);
+}
+*/
+
+//დავალება 5
+
+let sente = ''
+let lookingWord = ''
+
+
+
+//დავალება 6
+/*
+let numberFirst = 6;
+let numberSecond = 10
+
+alert(Math.min(numberFirst, numberSecond))*/
+
+//დავალება7 
+/*
+let numberFirst = 6;
+let numberSecond = 10
+
+alert(Math.max(numberFirst, numberSecond))
+*/
