e8215ebce55056f2349cf9ca0ce9345723149f76
Ljojua15 <jojualasha15@gmail.com>
2024-02-26 23:52:30 +0400

    start

---
+++ app3.js
@@ -0,0 +1,12 @@
+let arr = [
+    { name: 'lსდფasha', age: 533425, profesion: 'qa' },
+    { name: 'laდფსსდფsha', age: 13225, profesion: 'qaსდფს' },
+    { name: 'სდფსlasha', age: 565425, profesion: 'qaდსფს' },
+    { name: 'lashსდფსa', age: 3232425, profesion: 'qaსდფსასდასდ' },
+    { name: 'სსფს', age: 2534222, profesion: 'qaსდფსასდასდასფ' },
+    { name: 'სდფსდ', age: 253234, profesion: 'საფასდადqa' },
+
+]
+
+let newarr = arr.filter(x => x.age > 1000)
+console.log(newarr)
