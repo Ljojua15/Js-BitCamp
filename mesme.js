104b2605cb04b65777597a7558ecc775d72fa982
Ljojua15 <jojualasha15@gmail.com>
2024-04-02 08:40:10 +0400

    sd

---
+++ mesme.js
@@ -0,0 +1,67 @@
+//1
+/*
+let i=-1
+while(i<10){
+    i = i+1
+    console.log(i)
+}
+
+
+for (let i = 0; i <11; i = i+1){
+    console.log(i)
+}*/
+
+//2
+/*
+for (let i =0; i <20; i+=2){
+    console.log(i)
+}*/
+
+//3
+/*
+let ask = Number(prompt('შეიყვანეთ რიცხვი'))
+for (let i=0; i < ask; i++){
+    console.log('hello')
+}*/
+
+//4
+
+/*
+let x = 0;
+for(let i = 1; i<100; i++){
+    x += i
+}
+console.log(x)
+*/
+
+//5
+/*
+let result = 0;
+for (let i = 1; i <100; i+=2){
+    result =result + i
+}
+
+console.log(result)
+*/
+
+//6
+//
+//
+// let asked;
+// do{
+//     asked = Number(prompt('შეიყვანეთ რიცხცი 1 დან 10 მდე'))
+// }while (asked !==4)
+//
+//
+
+
+const qqq = function (x){
+    return (x * x)
+}
+
+console.log(qqq(2))
+console.log(qqq(5))
+
+let number = qqq(6)
+
+console.log(4 + number)
