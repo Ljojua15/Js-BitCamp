fa2af43145ba5d314358299aba2498d061e073bd
Ljojua15 <jojualasha15@gmail.com>
2024-04-10 11:10:38 +0400

    6

--- meotxe.js
+++ meotxe.js
@@ -29,20 +29,24 @@ console.log(separate())
 
 // console.log(convert())
 
-function convert() {
-    let str = prompt('ხასიათი და სმაილი');
-    let happ = ':)';
-    let sad = ":(";
-    let repHapp = 'icinis';
-    let repSad = 'tiris';
-
-    if (str.includes(happ)) {
-        return str.replace(new RegExp(/\b/ + happ + "/\b/", "gi"), repHapp);
-    } else if (str.includes(sad)) {
-        return str.replace(new RegExp("\\b" + sad + "\\b", "gi"), repSad);
-    } else {
-        return str; // Return the original string when there are no emoticons
-    }
-}
-
-console.log(convert());
+// function convert() {
+//     let str = prompt('ხასიათი და სმაილი');
+//     let happ = ':)';
+//     let sad = ":(";
+//     let repHapp = 'icinis';
+//     let repSad = 'tiris';
+//
+//     if (str.includes(happ)) {
+//         str = str.replace(happ, repHapp)
+//     }
+//     if (str.includes(sad)) {
+//        str = str.replace(sad,repSad)
+//     }
+//         return  str
+//
+// }
+//
+//
+// console.log(convert())
+//
+//
